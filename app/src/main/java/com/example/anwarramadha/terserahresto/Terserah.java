1647e294d513f767fe7863b62e92ae06e38cc5db
Anwar Ramadha <anwar.ramadha@gmail.com>
2017-02-17 20:49:56 +0700

    initial commit

---
+++ app/src/main/java/com/example/anwarramadha/terserahresto/Terserah.java
@@ -0,0 +1,13 @@
+package com.example.anwarramadha.terserahresto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Terserah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terserah);
+    }
+}
